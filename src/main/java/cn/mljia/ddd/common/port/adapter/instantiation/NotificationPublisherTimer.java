06d29183d9d188aaea6868c36ad0c856a6a2ac96
Administrator <Administrator@WIN-A3NOR7QF3AS.lan>
2017-03-27 19:56:49 +0800

    修改事件跟踪process线程中断问题

--- src/main/java/cn/mljia/ddd/common/port/adapter/instantiation/NotificationPublisherTimer.java
+++ src/main/java/cn/mljia/ddd/common/port/adapter/instantiation/NotificationPublisherTimer.java
@@ -1,5 +1,9 @@
 package cn.mljia.ddd.common.port.adapter.instantiation;
 
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -22,8 +26,10 @@ public class NotificationPublisherTimer extends Thread
     
     private MessageProducer messageProducer;
     
+    private String lockName;
+    
     public NotificationPublisherTimer(String typeName, NotificationApplicationService notificationApplicationService,
-        Lock lock, String domainName,MessageProducer messageProducer)
+        Lock lock, String domainName, MessageProducer messageProducer)
     {
         this.setTypeName(typeName);
         
@@ -34,63 +40,92 @@ public class NotificationPublisherTimer extends Thread
         this.setDomainName(domainName);
         
         this.setMessageProducer(messageProducer);
+        
+        this.setLockName(this.getDomainName() + "_" + this.getTypeName());
     }
     
     @Override
     public void run()
     {
-        while (true)
+        this.setName(this.getDomainName() + "_" + this.getTypeName());
+        if (logger.isDebugEnabled())
         {
+            DateFormat dateFormat = new SimpleDateFormat("yy-MM-dd HH:mm:ss");
+            logger.debug("Notification Message Tracker Name======>{},currentTime:====>{}",
+                this.getTypeName(),
+                dateFormat.format(new Date()));
+        }
+        
+        try
+        {
+            boolean tryLock = false;
             try
             {
-                boolean tryLock = false;
-                try
+                tryLock = this.getLock().tryLock(this.getLockName());
+                if (tryLock)
                 {
-                    tryLock = this.getLock().tryLock(this.getDomainName()+"_"+this.getTypeName());
-                    if (tryLock)
+                    if (logger.isDebugEnabled())
                     {
-                        this.getNotificationApplicationService().publishNotifications(this.getMessageProducer(),this.getTypeName());
-                        logger.debug(Thread.currentThread().getName() + "--->" + "resource lock success");
+                        logger.debug("thread-name:=====>{},lockName:=======>{},tryLock-status:=======>{}",
+                            Thread.currentThread().getName(),
+                            lockName,
+                            tryLock);
                     }
                     else
                     {
-                        logger.debug(Thread.currentThread().getName() + "--->" + "resource lock failed");
+                        this.getNotificationApplicationService().publishNotifications(this.getMessageProducer(),
+                            this.getTypeName());
                     }
+                    
                 }
-                catch (Exception e)
+                else
                 {
-                    logger.error("tryLock resource is error :" + e.getMessage(), e);
-                }
-                finally
-                {
-                    if (tryLock)
+                    if (logger.isDebugEnabled())
                     {
-                        if (this.getLock().unLock(this.getDomainName()+"_"+this.getTypeName()))
-                        {
-                            logger.debug(Thread.currentThread().getName() + "--->" + "resource unlock success");
-                        }
-                        else
-                        {
-                            logger.debug(Thread.currentThread().getName() + "--->" + "resource unlock failed");
-                        }
+                        logger.debug("thread-name:=====>{},lockName:=======>{},tryLock-status:=======>{}",
+                            Thread.currentThread().getName(),
+                            lockName,
+                            tryLock);
                     }
                 }
-                
             }
             catch (Exception e)
             {
-                e.printStackTrace();
-                System.out.println("Problem publishing notifications from ApplicationServiceLifeCycle.");
+                logger.debug("current Tracker Name======>{}", this.getTypeName());
+                logger.error("tryLock resource is error :" + e.getMessage(), e);
+            }
+            finally
+            {
+                if (tryLock)
+                {
+                    if (this.getLock().unLock(this.getLockName()))
+                    {
+                        if (logger.isDebugEnabled())
+                        {
+                            logger.debug("thread-name:=====>{},lockName:=======>{},unLock-status:=======>{}",
+                                Thread.currentThread().getName(),
+                                lockName,
+                                "success");
+                        }
+                    }
+                    else
+                    {
+                        if (logger.isDebugEnabled())
+                        {
+                            logger.debug("thread-name:=====>{},lockName:=======>{},unLock-status:=======>{}",
+                                Thread.currentThread().getName(),
+                                lockName,
+                                "fail");
+                        }
+                    }
+                }
             }
             
-            try
-            {
-                Thread.sleep(200L);
-            }
-            catch (Exception e)
-            {
-                // ignore
-            }
+        }
+        catch (Exception e)
+        {
+            e.printStackTrace();
+            System.out.println("Problem publishing notifications from ApplicationServiceLifeCycle.");
         }
     }
     
@@ -118,33 +153,40 @@ public class NotificationPublisherTimer extends Thread
     {
         this.lock = lock;
     }
-
+    
     private Lock getLock()
     {
         return lock;
     }
-
+    
     private String getDomainName()
     {
         return domainName;
     }
-
+    
     private MessageProducer getMessageProducer()
     {
         return messageProducer;
     }
-
+    
     private void setMessageProducer(MessageProducer messageProducer)
     {
         this.messageProducer = messageProducer;
     }
-
+    
     private void setDomainName(String domainName)
     {
         this.domainName = domainName;
     }
     
+    private String getLockName()
+    {
+        return lockName;
+    }
     
-    
+    private void setLockName(String lockName)
+    {
+        this.lockName = lockName;
+    }
     
 }
