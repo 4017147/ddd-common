b9f5a53fb91ae67daaf37a5289bc8536dbc5f5d6
DESKTOP-I9MCGGR\marker <lyl@lcjh.com>
2017-10-24 09:41:37 +0800

    no message

--- src/main/java/cn/mljia/ddd/common/media/SqlConfig.java
+++
@@ -1,5 +0,0 @@
-package cn.mljia.ddd.common.media;
-
-public class SqlConfig {
-	   public static final Integer LIMIT = 100;
-}
--- src/main/java/cn/mljia/ddd/common/port/adapter/messaging/RoutingKeys.java
+++
@@ -1,6 +0,0 @@
-package cn.mljia.ddd.common.port.adapter.messaging;
-
-public class RoutingKeys {
-
-	public static final String DEAD_LETTER_ROUTING_KEY = "cn.mljia.ha.dlq.domain.rk";
-}
